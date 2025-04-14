2b4a7a1c0e24562c7c15a814a8d1fb3db17fb275
motasimdev <themotasimdev@gmail.com>
2025-04-15 01:00:10 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let name = "Motasim Billah";
+name = "Md Motasim Billah";
+
+console.log("Amar nam" + " " + name);
+
+let x = 5;
+let y = 5;
+{
+  let age = x * y;
+  console.log("Amar age" + " " + age);
+}
+
+let namta = 1;
+let final_namta = namta + " " + "x" + " " + namta + " " + "=" + " " + namta;
+
+console.log(final_namta);
