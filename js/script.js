6f4ff7c56b55b1999ec787b4edc9487a31137c2c
motasimdev <themotasimdev@gmail.com>
2025-04-17 15:22:25 +0600

    aa

--- js/script.js
+++ js/script.js
@@ -1,16 +1,111 @@
-let name = "Motasim Billah";
-name = "Md Motasim Billah";
+// basic if...else
 
-console.log("Amar nam" + " " + name);
+// input
+let age = 18;
+// input
 
-let x = 5;
-let y = 5;
-{
-  let age = x * y;
-  console.log("Amar age" + " " + age);
+if (age >= 18) {
+  console.log("Tomar Biyar Boyosh Hoise");
+} 
+
+else {
+  console.log("Tmi Ekhno Picchi");
 }
 
-let namta = 1;
-let final_namta = namta + " " + "x" + " " + namta + " " + "=" + " " + namta;
+// basic if...else
 
-console.log(final_namta);
+// nested if...else
+
+// input
+let gender = "male";
+let boyosh = 25;
+let area = "noakhali";
+// input
+
+if (gender == "male") {
+  // 1st if
+  if (boyosh >= 25) {
+    // 2nd if
+    if (area == "noakhali") {
+      // 3rd if
+      console.log("tumi pass");
+    } else {
+      console.log("noakhalir hote hbe");
+    }
+  } else {
+    console.log("boyosh mile nai");
+  }
+} else {
+  console.log("male hote hbe");
+}
+
+// nested if...else
+
+// else...if
+
+// input
+let email = "yoyo@gmail.com";
+let id = 3344;
+let pass = 2233;
+// input
+
+if (email == "yoyo@gmail.com") {
+  console.log("email milse");
+} 
+
+else if (id == 3344) {
+  console.log("id milse");
+} 
+
+else if (pass == 2233) {
+  console.log("pass milse");
+} 
+
+else {
+  console.log("shob vul");
+}
+
+// else...if
+
+// marksheet
+
+// input
+let marks = 10;
+// input
+
+if (marks <0 || marks >100) {
+  console.log("eshb chalaki cholbe na");
+  
+}
+
+else if (marks >= 33 && marks < 40) {
+  console.log("grad D");
+} 
+
+else if (marks >= 40 && marks <50){
+  console.log("grad C");
+  
+}
+else if (marks >= 50 && marks <60){
+  console.log("grad B");
+  
+}
+else if (marks >= 60 && marks <70){
+  console.log("grad A-");
+  
+}
+else if (marks >= 60 && marks <70){
+  console.log("grad A");
+  
+}
+else if (marks >= 80 && marks <=1000){
+  console.log("grad A+");
+  
+}
+
+else {
+  console.log("fail");
+}
+
+// marksheet
+
